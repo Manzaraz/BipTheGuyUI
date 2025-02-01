0d5d84f57716af86857dfb321452d3eb9b7810d4
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-01 16:24:11 -0300

    Initial Commit

---
+++ BipTheGuyUI/BipTheGuyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyUIApp.swift
+//  BipTheGuyUI
+//
+//  Created by Christian Manzaraz on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
